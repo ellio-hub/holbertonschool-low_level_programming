c8f55513830a23fd6bc965bed3ccbf8411078469
ali mansour <1421@holbertonschool.com>
2020-02-11 10:39:35 +0100

    push me

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
-#include <betty.h>
+#include "holberton.h"
 /**
- *main - prints something
- * Return: 0
- */
+* main - writes Holberton
+* @void: empty
+* Return: 0
+*/
 int main(void)
 {
-  _putchar("H");
-  _putchar("o");
-  _putchar("l");
-  _putchar("b");
-  _putchar("e");
-  _putchar("r");
-  _putchar("t");
-  _putchar("o");
-  _putchar("n");
-  _putchar("\n");
+_putchar ('H');
+_putchar ('o');
+_putchar ('l');
+_putchar ('b');
+_putchar ('e');
+_putchar ('r');
+_putchar ('t');
+_putchar ('o');
+_putchar ('n');
+_putchar ('\n');
 return (0);
 }
