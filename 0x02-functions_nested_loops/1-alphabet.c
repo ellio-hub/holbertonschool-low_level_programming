fec4f26aa362d1a9ddda7426a43a9543bd5982db
ali mansour <1421@holbertonschool.com>
2020-02-11 11:33:58 +0100

    push me

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - prints the alphabet
+* @ch: char
+* Return: 0
+*/
+
+int main () {
+   char ch;
+
+   for(ch = 'A' ; ch <= 'Z' ; ch++) {
+      putchar(ch);
+   }
+   
+   return(0);
+}
