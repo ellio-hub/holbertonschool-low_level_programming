22306552d93fabe2b9d5e4f937050233cdf2a87f
ali mansour <1421@holbertonschool.com>
2020-02-12 12:31:50 +0100

    push me

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_sign - main function
+ * @n: integer
+ * Return: 0
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
+_putchar('\n');
+}
