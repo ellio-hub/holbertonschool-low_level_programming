6e5b925d9994fb10e8b38692642619f5942308e2
ali mansour <1421@holbertonschool.com>
2020-02-13 12:59:02 +0100

    push me

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _isdigit - main function
+ * @c: int
+ * Return: 0
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+return (1);
+else
+return (0);
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -2,4 +2,5 @@
 #define HOLBERTON_H
 int _putchar(char c);
 int _isupper(int c);
+int _isdigit(int c);
 #endif
