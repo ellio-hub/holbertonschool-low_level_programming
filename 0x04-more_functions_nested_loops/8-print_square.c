3722d06459dad252226de446e21de3e9835434d4
ali mansour <1421@holbertonschool.com>
2020-02-13 22:40:18 +0100

    push me

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * print_square - print square
+ * @size: square size
+ *
+ * Return: 0
+ */
+void print_square(int size)
+{
+int i;
+int j;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
