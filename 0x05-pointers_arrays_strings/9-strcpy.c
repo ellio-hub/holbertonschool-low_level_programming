aedf49353d9370a8c287dec3bf28c77029004cce
ali mansour <1421@holbertonschool.com>
2020-02-18 23:29:14 +0100

    push me

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strcpy - copy a string
+ * @source: parameter
+ * @dest: parameter
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+dest[i] = '\0';
+return (dest);
+}
