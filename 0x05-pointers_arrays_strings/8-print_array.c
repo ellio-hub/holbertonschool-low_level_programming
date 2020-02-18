627e70cd57175912a5ea10b2834efba1edf6ddb0
ali mansour <1421@holbertonschool.com>
2020-02-18 23:21:00 +0100

    push me

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - print an array
+ * @a: parameter
+ * @n: parameter
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i < n - 1)
+printf(", ");
+}
+printf("\n");
+}
