137ab36e5020346c1c26e990ab837d64bdaedce7
ali mansour <1421@holbertonschool.com>
2020-02-18 22:52:52 +0100

    push me

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * puts_half - prints half of a string
+ * @str: parameter
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+int l;
+int i;
+int x;
+for (l = 0; str[l] != '\0'; l++)
+x = (l + 1) / 2;
+for (i = x; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
