df01f0a9b77d1c5122f1035d8607ad27f83fc723
ali mansour <1421@holbertonschool.com>
2020-02-21 12:36:44 +0100

    push me

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _strcmp - compare 2 strings
+ * @s1: parameter
+ * @s2: parameter
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+{
+s1++;
+s2++;
+}
+return (*s1 - *s2);
+}
