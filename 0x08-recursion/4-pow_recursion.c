6f34b8b709e0d334345e650f9731ff134c23a6a6
ali mansour <1421@holbertonschool.com>
2020-02-26 14:29:44 +0100

    push me

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - main function
+ * @x: parameter
+ * @y: parameter
+ *
+ * Return: int
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+if (y == 0)
+return (1);
+ return (x * _pow_recursion(x, y - 1));
+}
