118fcd0f61107512d4b0f488656d250270960238
ali mansour <1421@holbertonschool.com>
2020-03-23 19:27:19 +0100

    push me

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,10 +8,11 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+unsigned int i;
 int sum = 0;
 va_list args;
 va_start(args, n);
-for (unsigned int i = 0; i < n; i++)
+for (i = 0; i < n; i++)
 sum += va_arg(args, int);
 return (sum);
 }
