10ed5d85f9ee68f6d2fdc16b9b320d5b5b24d41a
ali mansour <1421@holbertonschool.com>
2020-03-23 20:52:59 +0100

    push me

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,10 +10,10 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-va_list args;
-va_start(args, n);
 unsigned int i;
 int num;
+va_list args;
+va_start(args, n);
 for (i = 0; i < n; i++)
 {
 num = va_arg(args, int);
