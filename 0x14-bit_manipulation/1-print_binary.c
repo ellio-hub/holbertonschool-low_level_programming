f79d74a8f971a9f32d76f838461c8690db7ed9db
ali mansour <1421@holbertonschool.com>
2020-03-26 20:30:32 +0100

    push me

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,13 +7,13 @@
  */
 void print_binary(unsigned long int n)
 {
-unsigned long int m=32768;
-while(m > 0)
-   {
-   if((n & m) == 0 )
-         _putchar('0');
-   else
-         _putchar('1');
-  m = m >> 1;
-   }
+unsigned long int m = 32768;
+while (m > 0)
+{
+if ((n & m) == 0)
+_putchar('0');
+else
+_putchar('1');
+m = m >> 1;
+}
 }
