f950fec0bf65642c1a260bde8b1a830cac1241c0
ali mansour <1421@holbertonschool.com>
2020-03-26 16:12:37 +0100

    push me

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * binary_to_uint - main function
+ * @b: parameter
+ *
+ * Retun: 0 or converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+int i = 0;
+int l;
+int q = 1;
+unsigned int x = 0;
+for (l = 0; b[l] != '\0'; l++)
+{
+if ((b[l] != '0') && (b[l] != '1'))
+return (0);
+}
+if (l == 0)
+return (0);
+for (i = (l - 1); i >= 0; i--)
+{
+x += ((b[i] - 48) * q);
+q *= 2;
+}
+return (x);
+}
