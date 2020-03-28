c9e30420d0d977cfb66515fe593767938226ffc9
ali mansour <1421@holbertonschool.com>
2020-03-28 18:07:01 +0100

    push me

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * clear_bit - main function
+ * @n: parameter
+ * @index: parameter
+ *
+ * Return: int
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+unsigned long int x = 1;
+if (*n >> index)
+{
+x = x << index;
+*n = (*n ^ x);
+return (1);
+}
+else
+return (-1);
+}
