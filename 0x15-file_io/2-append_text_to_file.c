de49794516c7a35ca474ff9a1eb95ffe5a194963
ali mansour <1421@holbertonschool.com>
2020-03-31 20:26:28 +0100

    push me

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * append_text_to_file - main function
+ * @filename: parameter
+ * @text_content: parameter
+ *
+ * Return: int
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+int op, i ,c;
+if (!filename)
+return (-1);
+op = open(filename, O_WRONLY | O_APPEND);
+if (op == -1)
+return (-1);
+if (text_content)
+{
+for (i = 0; text_content[i]; i++)
+;
+c = crite(op, text_content, i);
+if (c == -1)
+return (-1);
+}
+close(op);
+return (1);
+}
