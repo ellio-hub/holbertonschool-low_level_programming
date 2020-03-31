0d0ce274848cc006e743b80e25e7abce53f8a8f5
ali mansour <1421@holbertonschool.com>
2020-03-31 23:53:47 +0100

    push me

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,11 +1,12 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include <string.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
