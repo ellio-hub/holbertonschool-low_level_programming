9e952706e32dacc0e376192db0786c4f563506f7
ali mansour <1421@holbertonschool.com>
2020-03-31 20:09:55 +0100

    push me

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,13 +10,14 @@ int create_file(const char *filename, char *text_content)
 int op, i, c;
 if (!filename)
 return (-1);
-op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+op = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 if (op == -1)
 return (-1);
 if (text_content)
 {
 for (i = 0; text_content[i]; i++)
 ;
+c = write(op, text_content, i);
 if (c == -1)
 return (-1);
 }
