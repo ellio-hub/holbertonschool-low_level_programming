102577775284b7d1aba87c8ce8dd93b8fe16cddf
ali mansour <1421@holbertonschool.com>
2020-03-31 19:32:00 +0100

    push me

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,12 +1,10 @@
 #include "holberton.h"
-
 /**
  * read_textfile - main function
- * @filename : parameter
- * @letters : parameter
+ * @filename: parameter
+ * @letters: parameter
  * Return: ssize_t
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 char *ch;
