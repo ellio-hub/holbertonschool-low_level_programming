1167d42b6377eebdc8c6005c56540ec5eff9ca76
ali mansour <1421@holbertonschool.com>
2020-03-31 19:55:13 +0100

    push me

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * create_file - main function
+ * @filename: parameter
+ * @text_content: parameter
+ * Return: int
+ */
+int create_file(const char *filename, char *text_content)
+{
+int op, i, c;
+if (!filename)
+return (-1);
+op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (op == -1)
+return (-1);
+if (text_content)
+{
+for (i = 0; text_content[i]; i++);
+c = write(op, text_content, i);
+if (c == -1)
+return (-1);
+}
+return (1);
+}
