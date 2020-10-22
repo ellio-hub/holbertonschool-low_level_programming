ad9af7b701ec86497e9e9826fc2afb2364811b62
ali mansour <1421@holbertonschool.com>
2020-10-22 21:27:54 +0100

    1st push

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print the name of the function
+ * @argc: arguments count
+ * @argv: arguments value
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+printf("%s\n", argv[0]);
+return (0);
+}
