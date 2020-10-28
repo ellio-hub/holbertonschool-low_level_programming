49990818c6669872d5da4ab25e6369c6387f2a4c
ali mansour <1421@holbertonschool.com>
2020-10-28 23:51:01 +0100

    ...

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,10 +1,11 @@
 section .data
-message db "Hello, Holberton", 10
+message db "Hello, Holberton",10
 section .text
-global _start
-_start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, message
-    mov rdx, 14
-    syscall
+global main
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, message
+	mov rdx, 17
+	syscall
+    
