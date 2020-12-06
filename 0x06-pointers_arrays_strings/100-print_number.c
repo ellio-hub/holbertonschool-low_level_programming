80f8b8799c66de0a1609aa577c564e85f5eab893
ali mansour <1421@holbertonschool.com>
2020-12-06 17:07:08 +0100

    1st push

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_number - print a number
+ * @n: parameter
+ * Return: void
+ */
+
+void print_number(int n)
+{
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+if (n/10)
+print_number(n/10);
+_putchar(n%10 + '0');
+}
