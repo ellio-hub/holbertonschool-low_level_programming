e22d614a78675039aa43d461d40d330d92d405a7
ellio-hub <mansourali1919@gmail.com>
2020-12-06 23:21:53 +0100

    3rd push

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - main function
+ * @s1: parameter
+ * @s2: p   r   meter
+ * Return: integer
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+if (*s1 == '\0' && *s2 == '\0')
+return (1);
+else if (*s1 == '\0' && *s2 == '*')
+return (wildcmp(s1, (s2 + 1)));
+else if (*s1 == '\0' || *s2 == '\0')
+return (0);
+if (*s1 == *s2)
+return (wildcmp((s1 + 1), (s2 + 1)));
+if (*s2 == '*')
+{
+if (*(s2 + 1) == '*')
+return (wildcmp(s1, (s2 + 1)));
+else if (wildcmp(s1, s2 + 1))
+return (1);
+else
+return (wildcmp(s1 + 1, s2));
+}
+return (0);
+}
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,21 +1,32 @@
 #include "holberton.h"
 #include <stdio.h>
+
 /**
  * _sqrt_recursion - main function
  * @n: parameter
  *
  * Return: int
  */
+
 int _sqrt_recursion(int n)
 {
-int x = n;
-if (n < 0)
-return (-1);
-if (n == 0 || n == 1)
-return (1);
- if (((x * x) - n) < 1)
-return (x);
-x = _sqrt_recursion(x);
-printf("x = %d",x);
-return (0.5 * (x + n / x));
+if (n == 0)
+return (0);
+return (check(1, n));
+}
+
+/**
+ * check - check function
+ * @i:parameter
+ * @j:parameter
+ * return: integer
+ */
+
+int check(int i, int j)
+{
+	if (i * i == j)
+		return (i);
+	if (i * i > j)
+		return (-1);
+	return (check(i + 1, j));
 }
