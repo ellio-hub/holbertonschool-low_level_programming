b9922827edbfc765f3a04e06eb0397f00ce16876
ali mansour <1421@holbertonschool.com>
2020-12-06 20:34:17 +0100

    1st push

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -10,7 +10,46 @@
 
 void print_buffer(char *b, int size)
 {
-int i;
-for (i = 0; i < size; i++)
-sprintf(&b[2 * i], "%02X", b[i]);
+int i, x, j;
+if (size <= 0)
+printf("\n");
+else
+{
+for (i = 9, x = 0; i < size; i += 10)
+{	printf("%.8x: ", x);
+for (j = x; j <= i ; j++)
+{
+printf("%.2x", b[j]);
+if (j % 2 == 1)
+printf(" ");
+}
+for (j = x; j <= i ; j++)
+{
+if (b[j] < 32)
+printf(".");
+else
+printf("%c", b[j]);	}
+printf("\n"); x = i + 1;	}
+if  x < size - 1)
+{
+i -= 9;
+printf("%.8x: ", i);
+for (j = i; j < i + 10; j++)
+{
+if (j < size)
+printf("%.2x", b[j]);
+else
+printf("  ");
+if (j % 2 == 1)
+printf(" ");	}
+for (; i < size ; i++)
+{
+if (b[i] < 32)
+printf(".");
+else
+printf("%c", b[i]);
+}
+printf("\n");
+}
+}
 }
