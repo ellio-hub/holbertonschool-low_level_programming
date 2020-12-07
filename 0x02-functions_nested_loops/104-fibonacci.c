e3f622aea9c57a18ce54f76a73fd128a6a3668b2
ellio-hub <mansourali1919@gmail.com>
2020-12-07 01:06:38 +0100

    5th push

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,16 +7,16 @@
 
 int main(void)
 {
-long int a = 0, b = 1, s;
+unsigned long int a = 0, b = 1, s;
 int i;
 for (i = 1; i < 98; i++)
 {
 s = a + b;
 a = b;
 b = s;
-printf("%ld, ", s);
+printf("%lu, ", s);
 }
 s = a + b;
-printf("%ld\n", s);
+printf("%lu\n", s);
 return (0);
 }
